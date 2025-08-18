6389795c8b559414755e2a32854232f292f96e3b
amerigovegliante <veglianteamerigo583@gmail.com>
2025-08-18 17:35:04 +0200

    Add files via upload

---
+++ graphs/clustered_graph.m
@@ -0,0 +1,11 @@
+function [] = clustered_graph(clustered_results) 
+    figure;
+    imagesc(clustered_results);
+    colormap([1 0 0; 0 1 0]);
+    colorbar('Ticks',[0,1],'TickLabels',{'Fail','Success'});
+    xlabel('Multiplicity');
+    ylabel('Delta index (smaller delta = more clustered)');
+    yticks(1:4);
+    yticklabels({'1e-1','1e-2','1e-3','1e-4'}); 
+    title('Clustered Eigenvalues: Diagonalization Success');
+end
---
+++ graphs/spaced_graph.m
@@ -0,0 +1,9 @@
+function [] = spaced_graph(spaced_results) 
+    figure;
+    bar(1:7, spaced_results, 'FaceColor',[0 1 0]);
+    xlabel('Multiplicity');
+    ylabel('Diagonalization Success (1 = success, 0 = fail)');
+    title('Well Spaced Eigenvalues: Diagonalization Success vs Multiplicity');
+    ylim([-0.2 1.2]);
+    grid on;
+end
