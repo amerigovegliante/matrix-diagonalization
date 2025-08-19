8dd09b713f08821bdd5cd044a3a5dca6903eca6f
amerigovegliante <veglianteamerigo583@gmail.com>
2025-08-19 13:09:42 +0200

    added comments, factorized code, deleted useless old lines

--- functions/myobjective.m
+++ functions/myobjective.m
@@ -1,24 +1,24 @@
 function [f,g] = myobjective(z,A)
 
-    if size(A,1) ~= size(A,2)
-        error("The matrix A is not squared!");
+    if size(A,1) ~= size(A,2)                                   % matrix size integrity check
+        error("The matrix A is not squared!");                  
     end
         
-    B = A - z * eye(size(A));   % A = B - zI
+    B = A - z * eye(size(A));                                   % A = B - zI
 
-    [L,U,P] = lu(B);            % lu factorization
+    [L,U,P] = lu(B);                                            % lu factorization with partial pivoting
 
-    detU = prod(diag(U));       
-    detP = det(P);
-    detB = detU / detP;
+    detU = prod(diag(U));                                       % detU is the product of it's diagonal values
+    detP = det(P);                                              % detP = +1 or -1 because it's a permutation matrix
+    detB = detU / detP;                                         % detU = detB * detP
 
     f = detB;
 
     % B matrix inversion
 
-    id = eye(size(B));
-    y = L \ (P * id);
-    Binv = U \ y;
+    id = eye(size(B));                                          % n x n identity matrix
+    y = L \ (P * id);                                           % solve linear system Ly = Pid
+    Binv = U \ y;                                               % solve linear system UBinv = y
 
-    g = 1 / trace(Binv);
+    g = 1 / trace(Binv);                                        % g = trace(B^-1)
 end
