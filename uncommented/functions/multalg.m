41f5bc357183ba2ea11612dd84bad96abb0338d4
Amerigo Vegliante <veglianteamerigo583@gmail.com>
2025-08-20 21:43:03 +0200

    remove comments for presentation purposes

--- uncommented/functions/multalg.m
+++ uncommented/functions/multalg.m
@@ -1,42 +1,42 @@
 function [l,m,flag] = multalg(A,l0,toll,it,maxit)
 
-    if size(A,1) ~= size(A,2)                                                   % matrix size integrity check
+    if size(A,1) ~= size(A,2)                                                   
         error("The matrix A is not squared!");
     end
 
-    x0 = l0;                                                                    % newton method start
+    x0 = l0;                                                                    
 
     for i=1:it
 
-        [~,g] = myobjective(x0,A);                                              % g = -f/f'
+        [~,g] = myobjective(x0,A);                                              
         x1 = x0 + g;
 
-        steps(i) = abs(x1 - x0);                                                % keep all the step values for heuristics
+        steps(i) = abs(x1 - x0);                                                
 
-        if steps(i) < toll                                                      % tollerance check
+        if steps(i) < toll                                                      
             m=1;
             flag=1;           
             l=x1;
             return;
         end
 
-        x0 = x1;                                                                % next cycle
+        x0 = x1;                                                                
 
     end
 
-    m = round(1/(1-(steps(end)/steps(end-1))));                                 % heuristics
+    m = round(1/(1-(steps(end)/steps(end-1))));                                 
 
-    x0 = l0;                                                                    % modified newthon method start
+    x0 = l0;                                                                    
 
-    for n=1:9                                                                   % 10 * maxit cycle
-        for i=1:maxit                                                           % maxit cycle modified newton method
+    for n=1:9                                                                   
+        for i=1:maxit                                                           
 
             [~,g] = myobjective(x0,A);
             x1 = x0 + m*g;
 
             step = abs(x1 - x0);
 
-            if step < toll                                                      % step criteria check
+            if step < toll                                                      
                 flag=1;
                 l=x1;
                 return
@@ -46,11 +46,12 @@ function [l,m,flag] = multalg(A,l0,toll,it,maxit)
 
         end
 
-        m=m+1;                                                                  % increment multiplicity
+        m=m+1;                                                                  
 
     end
 
     l=x1;
     flag=0;
+
 
 end
